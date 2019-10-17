b24a14f56f2ee6cf7a8f593c926718a2ed8db8c5
lciesla <lciesla@nurt.pl>
2019-10-17 16:02:27 +0200

    konieczność zaokrąglania do 2 miejsc pop rzecinku

--- Pocztowy.UnitTests/OrderCalculatorUnitTests.cs
+++ Pocztowy.UnitTests/OrderCalculatorUnitTests.cs
@@ -15,6 +15,7 @@ namespace Pocztowy.UnitTests
         [InlineData(0, 0.5, 0)]
         [InlineData(0.1, 0.5, 0.05)]
         [InlineData(0.2, 0.5, 0.10)]
+        [InlineData(0.1, 0.5, 0.00)]
         public void PercentageDiscountCalculateStrategyTest(
             decimal unitPrice,
             decimal percentage,
