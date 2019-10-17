341eb53a0562d875434108fe8ff97dba8e9cafa8
Aleksander Jabłoński <a.jablonski@pocztowy.pl>
2019-10-17 15:37:57 +0200

    Olek2 - NoCanDiscountStartegy - Info

--- Pocztowy.Calculators/CanDiscountStrategies/NoCanDiscountStrategy.cs
+++ Pocztowy.Calculators/CanDiscountStrategies/NoCanDiscountStrategy.cs
@@ -5,6 +5,11 @@ namespace Pocztowy.Calculators
 {
     public class NoCanDiscountStrategy : ICanDiscountStrategy
     {
+        /// <summary>
+        /// brak promocji
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
         public bool CanDiscount(Order order) => false;
     }
 }
