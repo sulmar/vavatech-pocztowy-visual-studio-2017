dbb2c60661d28c117828362143a42ca7221fc7f0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2019-10-17 17:02:58 +0200

    Dodanie szablonu

---
+++ Templates/ConsoleAsync/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleAsync
+{
+    class Program
+    {
+
+        // <= 7.0
+        //static void Main(string[] args) 
+        //    => MainAsync(args).GetAwaiter().GetResult();
+        
+        static async Task MainAsync(string[] args)
+        {
+            await Console.Out.WriteLineAsync("Hello World!");
+        }
+    }
+}
