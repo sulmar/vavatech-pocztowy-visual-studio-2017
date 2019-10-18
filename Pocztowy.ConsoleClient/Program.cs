0279f2077d6f6d8a8925799048447c53b6b35407
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2019-10-18 11:51:13 +0200

    profilowanie

--- Pocztowy.ConsoleClient/Program.cs
+++ Pocztowy.ConsoleClient/Program.cs
@@ -4,6 +4,8 @@ using Pocztowy.IRepositories;
 using Pocztowy.Models;
 using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Pocztowy.ConsoleClient
 {
@@ -29,10 +31,69 @@ namespace Pocztowy.ConsoleClient
     }
 
     
+    public interface IReportBuilder
+    {
+        string Build();
+    }
+
+    public class StringReportBuilder : IReportBuilder
+    {
+        public string Build()
+        {
+            string report = string.Empty;
+
+            for (int i = 0; i < 100000; i++)
+            {
+                report += $"A{i}";
+            }
+
+            return report;
+
+        }
+    }
+
+    public class StringBuilderReportBuilder : IReportBuilder
+    {
+        public string Build()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                stringBuilder.Append($"A{i}");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
 
     class Program
     {
+        
+
         static void Main(string[] args)
+        {
+            IReportBuilder reportBuilder1 = new StringReportBuilder();
+
+            IReportBuilder reportBuilder2 = new StringBuilderReportBuilder();
+
+            Task t1 = Task.Run(() => reportBuilder1.Build());
+            Task t2 = Task.Run(() => reportBuilder2.Build());
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine("Completed.");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+
+
+            // Test();
+
+        }
+
+        private static void Test()
         {
             Console.WriteLine("Branch aszatkowski");
 
@@ -72,14 +133,13 @@ namespace Pocztowy.ConsoleClient
                 );
 
             ICalculateDiscountStrategy calculateDiscountStrategy = new FixedCalculateDiscountStrategy(10);
-    
+
             IDiscountCalculator orderCalculator
                 = new DiscountCalculator(canDiscountStrategy, calculateDiscountStrategy);
 
             decimal discount = orderCalculator.CalculateDiscount(order);
 
             Console.WriteLine(discount);
-
         }
 
         private static decimal Calculate(int qty, int unitprice)
